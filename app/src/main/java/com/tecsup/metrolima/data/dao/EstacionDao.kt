598766c6ffebd5fd603d91b5e26435f0c6bc09fb
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-19 15:17:34 -0500

    Creación de EstacionDao con métodos para insertar y listar estaciones.

---
+++ app/src/main/java/com/tecsup/metrolima/data/dao/EstacionDao.kt
@@ -0,0 +1,20 @@
+package com.tecsup.metrolima.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.tecsup.metrolima.data.model.Estacion
+
+@Dao
+interface EstacionDao{
+    //Inserta la lista de estaciones
+    @Insert
+    suspend fun insertarEstaciones(estaciones: List<Estacion>)
+
+    //Obtiene todas las estaciones que se almacenan
+    @Query("SELECT * FROM estaciones")
+    suspend fun getAllEstaciones(): List<Estacion>
+
+
+
+}
