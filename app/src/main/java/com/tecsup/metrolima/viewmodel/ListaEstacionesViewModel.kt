fd605272400199b386fd8b0174777998348f9673
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-20 21:24:10 -0500

    Insertando Estaciones.

--- app/src/main/java/com/tecsup/metrolima/viewmodel/ListaEstacionesViewModel.kt
+++ app/src/main/java/com/tecsup/metrolima/viewmodel/ListaEstacionesViewModel.kt
@@ -49,7 +49,106 @@ class ListaEstacionesViewModel(
             ),
             Estacion(
                 id = 2,
-                nombre = "Estación Gamarra",
+                nombre = "Estación Santa Rosa",
+                distrito = "San Juan de Lurigancho",
+                latitud = -12.00429, longitud = -76.96373,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_santarosa
+            ),
+            Estacion(
+                id = 3,
+                nombre = "San Martín",
+                distrito = "San Juan de Lurigancho",
+                latitud = -12.00958, longitud = -76.97084,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_sanmartin
+            ),
+            Estacion(
+                id = 4,
+                nombre = "San Carlos",
+                distrito = "San Juan de Lurigancho",
+                latitud = -12.01312, longitud = -76.97626,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_sancarlos
+            ),
+            Estacion(
+                id = 5,
+                nombre = "Los Postes",
+                distrito = "San Juan de Lurigancho",
+                latitud = -12.01811, longitud = -76.98313,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_lospostes
+            ),
+            Estacion(
+                id = 6,
+                nombre = "Los Jardines",
+                distrito = "San Juan de Lurigancho",
+                latitud = -12.02241, longitud = -76.98928,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_losjardines
+            ),
+            Estacion(
+                id = 7,
+                nombre = "Pirámide del Sol",
+                distrito = "San Juan de Lurigancho",
+                latitud = -12.02916, longitud = -76.99842,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_piramidedelsol
+            ),
+            Estacion(
+                id = 8,
+                nombre = "Caja de Agua",
+                distrito = "San Juan de Lurigancho",
+                latitud = -12.03718, longitud = -77.00791,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_cajadeagua
+            ),
+            Estacion(
+                id = 9,
+                nombre = "Presbítero Maestro",
+                distrito = "El Agustino",
+                latitud = -12.05756, longitud = -77.03523,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_presbiteromaestro
+            ),
+            Estacion(
+                id = 10,
+                nombre = "El Ángel",
+                distrito = "El Agustino",
+                latitud = -12.06108, longitud = -77.02948,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_elangel
+            ),
+            Estacion(
+                id = 11,
+                nombre = "Miguel Grau",
+                distrito = "Cercado de Lima",
+                latitud = -12.06748, longitud = -77.02334,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_miguelgrau
+            ),
+            Estacion(
+                id = 12,
+                nombre = "Gamarra",
+                distrito = "La Victoria",
+                latitud = -12.07611, longitud = -77.01617,
+                linea = "Línea 1 Metro",
+                horario = "L-S: 06:00 - 22:00, D/F: 06:00 - 21:00",
+                imagenCircularResId = R.drawable.estacion_gamarra
+            ),
+            Estacion(
+                id = 13,
+                nombre = "Arriola",
                 distrito = "La Victoria",
                 latitud = -12.06789, longitud = -77.02109,
                 linea = "Línea 1 Metro",
